a28f0fe7a526f2401311b9a1d68364489bcb4714
Volodymyr Korchemnyi <volodymyr.korchemnyi@gmail.com>
2023-04-20 00:27:48 +0300

    build: plug-in promises-aplus-test
    with help of adapter connect promises-aplus-test with promise-polyfill library.
    
    promise-polyfill library using temporary to config promises-aplus-test.

---
+++ test/adapter.js
@@ -0,0 +1,34 @@
+//var Promise = require('../dist/bundle.js')
+let Promise = require('promise-polyfill')
+
+let deferred = function () {
+    let resolve
+    let reject
+    let promise = new Promise(function (res, rej) {
+        resolve = res
+        reject = rej
+    })
+    return {
+        promise,
+        resolve,
+        reject,
+    }
+}
+
+let resolved = function (value) {
+    let d = deferred()
+    d.resolve(value)
+    return d.promise
+}
+
+let rejected = function (reason) {
+    let d = deferred()
+    d.reject(reason)
+    return d.promise
+}
+
+module.exports = {
+    deferred,
+    resolved,
+    rejected,
+}
