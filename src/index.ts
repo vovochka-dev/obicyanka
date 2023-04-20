995784bf2b55ff6eb673c048d83b5c21bc972c1d
Volodymyr Korchemnyi <volodymyr.korchemnyi@gmail.com>
2023-04-20 18:07:30 +0300

    feat: very basic implementation of Promise

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-class Obicyanka {
-    constructor() {}
+module.exports = class Promise<T> {
+    constructor(executor: (resolve: () => void, reject: () => void) => void) {
+        executor(this.onResolve, this.onReject)
+    }
+    onResolve() {}
+    onReject() {}
+    static resolve() {}
+    static reject() {}
 }
