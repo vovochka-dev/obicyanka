a8aa4e026018a876772616df066bca0260559886
Volodymyr Korchemnyi <volodymyr.korchemnyi@gmail.com>
2023-04-20 15:18:47 +0300

    build: uninstall webpack, setup typescript

--- test/adapter.js
+++ test/adapter.js
@@ -1,5 +1,5 @@
-//var Promise = require('../dist/bundle.js')
-let Promise = require('promise-polyfill')
+var Promise = require('../src/index.js')
+//let Promise = require('promise-polyfill')
 
 let deferred = function () {
     let resolve
